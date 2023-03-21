7fa95abde27cc01e2d85af1af198a95a008c5bbe
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-03-21 19:07:17 +0900

    Style: Button Style

--- lib/views/taxiDialog.dart
+++ lib/views/taxiDialog.dart
@@ -15,75 +15,96 @@ class TaxiDialog extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 350,
-      height: 150,
+      height: 165,
       decoration: BoxDecoration(
           color: Colors.white, borderRadius: BorderRadius.circular(15)),
       child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("새로운 버전이 출시되었습니다!\n정상적인 사용을 위해 앱을 업데이트 해주세요!",
+            const Padding(padding: EdgeInsets.all(15)),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  text: "새로운 ",
+                  style: GoogleFonts.roboto(
+                      textStyle: const TextStyle(
+                          color: Color(0xFF323232),
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold)),
+                  children: <TextSpan>[
+                    TextSpan(text: "버전"),
+                    TextSpan(
+                        text: "이 ",
+                        style: TextStyle(fontWeight: FontWeight.normal)),
+                    TextSpan(
+                        text: "출시", style: TextStyle(color: Color(0xFF6E3678))),
+                    TextSpan(
+                        text: "되었습니다!",
+                        style: TextStyle(fontWeight: FontWeight.normal))
+                  ]),
+            ),
+            Text("정상적인 사용을 위해 앱을 업데이트 해주세요.",
+                textAlign: TextAlign.center,
                 style: GoogleFonts.roboto(
-                    textStyle: TextStyle(
-                        color: Color(0xFF323232),
-                        fontSize: 15,
+                    textStyle: const TextStyle(
+                        color: Color(0xFF888888),
+                        fontSize: 10,
                         fontWeight: FontWeight.bold))),
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(15),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                OutlinedButton(
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.all(Size(150, 45)),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Color(0xFF6E3678)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          side: BorderSide(color: Colors.black),
-                        ),
-                      ),
-                    ),
-                    child: Text("업데이트 하러가기",
-                        style: GoogleFonts.roboto(
-                            textStyle: TextStyle(
-                                color: Colors.white,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold))),
-                    onPressed: () async {
-                      LaunchReview.launch(
-                          androidAppId: dotenv.get("ANDROID_APPID"),
-                          iOSAppId: dotenv.get("IOS_APPID"));
-                    }),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                ),
-                OutlinedButton(
-                    style: ButtonStyle(
-                      fixedSize: MaterialStateProperty.all(Size(150, 45)),
-                      backgroundColor:
-                          MaterialStateProperty.all<Color>(Color(0xFFFAF8FB)),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          side: BorderSide(color: Colors.black),
-                        ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0.5,
+                      fixedSize: const Size(150, 45),
+                      backgroundColor: const Color(0xFFFAF8FB),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                        side: const BorderSide(color: Colors.white),
                       ),
                     ),
                     child: Text("앱 종료하기",
                         style: GoogleFonts.roboto(
-                            textStyle: TextStyle(
-                                color: Color(0xFFEEEEEE),
+                            textStyle: const TextStyle(
+                                color: Color(0xFFC8C8C8),
                                 fontSize: 15,
-                                fontWeight: FontWeight.bold))),
+                                fontWeight: FontWeight.normal))),
                     onPressed: () async {
                       if (Platform.isIOS) {
                         exit(0);
                       } else {
                         SystemNavigator.pop();
                       }
+                    }),
+                const Padding(
+                  padding: EdgeInsets.all(10),
+                ),
+                OutlinedButton(
+                    style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(const Size(150, 45)),
+                      backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xFF6E3678)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12.0),
+                          side: const BorderSide(color: Colors.black),
+                        ),
+                      ),
+                    ),
+                    child: Text("업데이트 하러가기",
+                        style: GoogleFonts.roboto(
+                            textStyle: const TextStyle(
+                                color: Color(0xFFEEEEEE),
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold))),
+                    onPressed: () async {
+                      LaunchReview.launch(
+                          androidAppId: dotenv.get("ANDROID_APPID"),
+                          iOSAppId: dotenv.get("IOS_APPID"));
                     }),
               ],
             )
