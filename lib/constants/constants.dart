977257b3eed0f7c0c9b82acabd0ee54c9655f083
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-04-04 09:42:00 +0900

    Refactor: Upgrade Libraries Version

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -5,6 +5,6 @@ String address = dotenv.get("BACKEND_ADDRESS");
 
 final BaseOptions connectionOptions = BaseOptions(
   baseUrl: address,
-  connectTimeout: 15000,
-  receiveTimeout: 13000,
+  connectTimeout: Duration(seconds: 150),
+  receiveTimeout: Duration(seconds: 130),
 );
