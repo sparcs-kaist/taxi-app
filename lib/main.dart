ff0510656b6e8269029dac9ba25b60e5f0331e07
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-05-24 00:07:34 +0900

    Fix: Remove Fcm init at initial launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+<<<<<<< Updated upstream
+=======
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:permission_handler/permission_handler.dart';
+>>>>>>> Stashed changes
 import 'package:taxiapp/utils/fcmToken.dart';
 import 'package:taxiapp/utils/pushHandler.dart';
 import 'package:taxiapp/utils/token.dart';
@@ -18,7 +24,10 @@ late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
   await dotenv.load(fileName: ".env");
 
   if (Platform.isAndroid) {
@@ -27,7 +36,6 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-
   channel = const AndroidNotificationChannel(
     'taxi_channel',
     'taxi_notification',
@@ -45,9 +53,9 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(handleMessage);
 
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
+    alert: false,
+    badge: false,
+    sound: false,
   );
 
   // 사용자가 푸시 알림을 허용했는지 확인 후 권한요청
@@ -57,14 +65,10 @@ void main() async {
     badge: true,
     carPlay: false,
     criticalAlert: false,
-    provisional: false,
+    provisional: true,
     sound: true,
   );
-
   await Token().init();
-
-  await FcmToken().init();
-
   runApp(MyHome());
 }
 
