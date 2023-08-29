27d1d9d45ff3b451b2f66658a3c36cd044e7b78c
ybminm <superybm@outlook.kr>
2023-08-29 22:18:49 +0900

    Remove: unused library

--- lib/views/taxiDialog.dart
+++ lib/views/taxiDialog.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:open_store/open_store.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:taxiapp/constants/constants.dart';
