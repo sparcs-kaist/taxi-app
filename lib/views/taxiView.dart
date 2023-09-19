b5472d2c61b384194b9d04888889b1da89e57455
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-09-19 23:34:47 +0900

    Refactor: Split Event handler

--- lib/views/taxiView.dart
+++ lib/views/taxiView.dart
@@ -16,6 +16,7 @@ import 'package:taxiapp/constants/theme.dart';
 import 'package:taxiapp/hooks/useLoadCount.dart';
 import 'package:taxiapp/utils/fcmToken.dart';
 import 'package:taxiapp/utils/pushHandler.dart';
+import 'package:taxiapp/utils/registerEventHandler.dart';
 import 'package:taxiapp/utils/remoteConfigController.dart';
 import 'package:taxiapp/views/loadingView.dart';
 import 'package:taxiapp/utils/token.dart';
@@ -485,145 +486,16 @@ class TaxiView extends HookWidget {
                 },
                 onWebViewCreated: (InAppWebViewController webcontroller) async {
                   _controller.value = webcontroller;
-                  _controller.value?.addJavaScriptHandler(
-                    handlerName: "auth_update",
-                    callback: (arguments) async {
-                      // 로그인 해제 시 로그인 State 변경
-                      if (arguments == [{}]) {
-                        isLogin.value = false;
-                        return;
-                      }
-                      // 로그인 성공 시 / 기존 토큰 삭제 후 새로운 토큰 저장
-                      if (!isAuthLogin.value) {
-                        if (arguments[0]['accessToken'] != null &&
-                            arguments[0]['refreshToken'] != null) {
-                          await Token().deleteAll();
-                          await Token().setAccessToken(
-                              accessToken: arguments[0]['accessToken']);
-                          await Token().setRefreshToken(
-                              refreshToken: arguments[0]['refreshToken']);
-                          await FcmToken()
-                              .registerToken(arguments[0]['accessToken']);
-                          isAuthLogin.value = true;
-                        }
-                      }
-                      return;
-                    },
-                  );
-
-                  _controller.value?.addJavaScriptHandler(
-                      handlerName: "auth_logout",
-                      callback: (args) async {
-                        try {
-                          await FcmToken()
-                              .removeToken(Token().getAccessToken());
-                          await Token().deleteAll();
-                          await _cookieManager.deleteAllCookies();
-                          isLogin.value = false;
-                          isAuthLogin.value = false;
-                          await _controller.value?.loadUrl(
-                              urlRequest: URLRequest(
-                                  url: Uri.parse(RemoteConfigController()
-                                      .frontUrl
-                                      .toString())));
-                        } catch (e) {
-                          // TODO
-                          Fluttertoast.showToast(
-                              msg: "서버와의 연결에 실패했습니다.",
-                              toastLength: Toast.LENGTH_SHORT,
-                              textColor: toastTextColor,
-                              backgroundColor: toastBackgroundColor);
-                          isAuthLogin.value = false;
-                        }
-                      });
-
-                  _controller.value?.addJavaScriptHandler(
-                      handlerName: "try_notification",
-                      callback: (args) async {
-                        if (await Permission.notification.isGranted) {
-                          return true;
-                        } else {
-                          openAppSettings();
-                          Fluttertoast.showToast(
-                              msg: "알림 권한을 허용해주세요.",
-                              toastLength: Toast.LENGTH_SHORT,
-                              textColor: toastTextColor,
-                              backgroundColor: toastBackgroundColor);
-                          return false;
-                        }
-                      });
-
-                  _controller.value?.addJavaScriptHandler(
-                      handlerName: "clipboard_copy",
-                      callback: (args) async {
-                        if (Platform.isAndroid) {
-                          await Clipboard.setData(ClipboardData(text: args[0]));
-                        }
-                      });
-
-                  // Web -> App
-                  _controller.value?.addJavaScriptHandler(
-                      handlerName: "popup_inAppNotification",
-                      callback: (args) async {
-                        createOverlayNotification(
-                            title: args[0]['title'].toString(),
-                            subTitle: args[0]['subtitle'].toString(),
-                            content: args[0]['content'].toString(),
-                            button: {
-                              args[0]['button']['text'].toString():
-                                  (args[0]['button']['path'].toString() != "")
-                                      ? Uri.parse(
-                                          args[0]['button']['path'].toString())
-                                      : Uri.parse("")
-                            },
-                            imageUrl: (args[0]['type'].toString() ==
-                                    "default") //TODO: type showMaterialBanner 함수에서 관리
-                                ? Uri.parse(args[0]['imageUrl'].toString())
-                                : Uri.parse(""));
-                      });
-
-                  _controller.value?.addJavaScriptHandler(
-                      handlerName: "popup_instagram_story_share",
-                      callback: (args) async {
-                        if (args[0] == {}) {
-                          return false;
-                        }
-                        try {
-                          final Dio _dio = Dio();
-                          final backgroundResponse = await _dio.get(
-                              args[0]['backgroundLayerUrl'],
-                              options:
-                                  Options(responseType: ResponseType.bytes));
-                          final stickerResponse = await _dio.get(
-                              args[0]['stickerLayerUrl'],
-                              options:
-                                  Options(responseType: ResponseType.bytes));
-                          final backgroundFile = await File(
-                                  (await getTemporaryDirectory()).path +
-                                      "/background.png")
-                              .create(recursive: true);
-                          final stickerFile = await File(
-                                  (await getTemporaryDirectory()).path +
-                                      "/sticker.png")
-                              .create(recursive: true);
-                          await backgroundFile
-                              .writeAsBytes(backgroundResponse.data);
-                          await stickerFile.writeAsBytes(stickerResponse.data);
-
-                          await SocialShare.shareInstagramStory(
-                              appId: dotenv.get("FACEBOOK_APPID"),
-                              imagePath: stickerFile.path,
-                              backgroundResourcePath: backgroundFile.path);
-                          return true;
-                        } catch (e) {
-                          Fluttertoast.showToast(
-                              msg: "인스타그램 스토리 공유에 실패했습니다.",
-                              toastLength: Toast.LENGTH_SHORT,
-                              textColor: toastTextColor,
-                              backgroundColor: toastBackgroundColor);
-                          return false;
-                        }
-                      });
+                  if (_controller.value != null) {
+                    registerEventHandler(
+                        _controller.value!,
+                        isLogin,
+                        isAuthLogin,
+                        createOverlayNotification,
+                        _cookieManager,
+                        toastTextColor,
+                        toastBackgroundColor);
+                  }
                 },
                 onLoadStart: (controller, uri) async {
                   if (isFcmInit.value &&
